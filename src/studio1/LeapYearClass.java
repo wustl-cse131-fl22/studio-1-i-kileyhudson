3c9d6f4aee8efb6ce9bf23a7e669a40bf525bcb8
kileyhudson <kileyhudson@10.232.137.213>
2022-09-08 14:17:09 -0500

    studio 1

---
+++ src/studio1/LeapYearClass.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYearClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a year");
+		int year = in.nextInt();
+		
+			
+	}
+
+}
